bb36ab2d8eed55987f7107acbf8f83152a732cc0
Yitao Li <yitao@rstudio.com>
2021-11-23 00:36:23 +0000

    benchmark script + results for rand forest classifier
    Signed-off-by: Yitao Li <yitao@rstudio.com>

---
+++ rand-forest-classifier/rand-forest-classifier.R
@@ -0,0 +1,90 @@
+# NOTE:
+# The benchmark was run with {cuda.ml} installed using the following options
+#
+# ```
+# install.packages("cuda.ml", type="source", INSTALL_opts="--byte-compile")
+# ```
+
+library(cuda.ml)
+library(rbenchmark)
+library(reticulate)
+
+cuml <- import("cuml")
+
+runners <- cuml$benchmark$runners
+algorithms <- cuml$benchmark$algorithms
+datagen <- cuml$benchmark$datagen
+
+dataset_name <- "classification"
+dataset_n_samples <- 10000L
+dataset_n_features <- 10L
+
+cat("\nBEGIN: cuML vs. scikit-learn benchmark:\n")
+runners$run_variations(
+  algos = list(algorithms$algorithm_by_name("RandomForestClassifier")),
+  dataset_name = dataset_name,
+  bench_rows = list(dataset_n_samples),
+  bench_dims = list(dataset_n_features),
+  n_reps = 10L
+)
+cat("\nEND: cuML vs. scikit-learn benchmark:\n")
+
+data <- datagen$gen_data(
+  dataset_name = dataset_name,
+  dataset_format = "numpy",
+  n_samples = dataset_n_samples,
+  n_features = dataset_n_features
+)
+
+cuda_ml_input_x <- force(data[[1]])
+cuda_ml_input_y <- force(data[[2]])
+
+cat("\n\nBEGIN: {cuda.ml} benchmark\n")
+print(
+  benchmark(
+    {
+      # NOTE: using the R-equivalent of the parameters from
+      # https://github.com/rapidsai/cuml/blob/branch-21.10/python/cuml/benchmark/algorithms.py#L303
+      # here to ensure benchmarks done using {cuda.ml} will be comparible to those
+      # done using {reticulate}+cuML
+      cuda_ml_rand_forest(x = cuda_ml_input_x, y = cuda_ml_input_y, max_predictors_per_note_split = dataset_n_features, trees = 10L)
+    },
+    replications = 10
+  )
+)
+cat("\nEND: {cuda.ml} benchmark\n")
+
+# Sample output from running this benchmark on a 'p3.2xlarge' Amazon EC2 instance
+# using the Docker image at https://hub.docker.com/r/yitaoli/rapids-cuml-benchmark:
+#
+# ```
+# BEGIN: cuML vs. scikit-learn benchmark:
+# Running:
+#  RandomForestClassifier
+# Running RandomForestClassifier...
+# Finished all benchmark runs
+#                      algo  input   cu_time  cpu_time  cuml_acc   cpu_acc    speedup  n_samples  n_features
+# 0  RandomForestClassifier  numpy  0.028174  0.713797  0.991899  0.991899  25.335167      10000          10
+#
+# END: cuML vs. scikit-learn benchmark:
+#
+#
+# BEGIN: {cuda.ml} benchmark
+#                                                                                                                                      test
+# 1 {\n    cuda_ml_rand_forest(x = cuda_ml_input_x, y = cuda_ml_input_y, max_predictors_per_note_split = dataset_n_features, trees = 10)\n}
+#   replications elapsed relative user.self sys.self user.child sys.child
+# 1           10   0.339        1     0.738     0.07          0         0
+#
+# END: {cuda.ml} benchmark
+# ```
+#
+# Please notice the following:
+# - The 'cu_time' output from the above is the average time elapsed per run
+#   using the GPU-accelerated cuML kMeans implementation, which was around
+#   0.028174 s/run.
+# - The 'cpu_time' output from the above is the average time elapsed per run
+#   using the scikit-learn implementation (i.e., CPU-only), which was around
+#   0.713797 s/run.
+# - The 'elapsed' output from the above within the '{cuda.ml} benchmark' section
+#   is the total elapsed time for 10 runs using {cuda.ml}, so the time elapsed
+#   per run for {cuda.ml} was around 0.039 s/run.
